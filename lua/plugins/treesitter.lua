f718b9c92f5fddd52556ea41de75d96061911866
nboisvert <nicklay@me.com>
2025-03-07 22:32:22 -0500

    Add treesitters

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -9,7 +7,10 @@ return {
     ensure_installed = {
       "lua",
       "vim",
-      "elixir"
+      "elixir",
+      "typescript",
+      "javascript",
+      "nix"
     },
   },
 }
