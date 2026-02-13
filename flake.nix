81efa8ab8ebb378afa97dce94980582103e8bdc7
nicklayb <nicklay@me.com>
2026-02-13 14:05:21 -0500

    [v5] Update

--- flake.nix
+++ flake.nix
@@ -3,31 +3,60 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
 
     let
       systems = nixpkgs.lib.systems.flakeExposed;
-      pkgsFor = system: import nixpkgs { inherit system; };
+      pkgsFor =
+        system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      packages = pkgs: [
+        pkgs.nil
+        pkgs.nodejs
+        pkgs.ripgrep
+        pkgs.fd
+        pkgs.git
+        pkgs.tree-sitter
+        pkgs.claude-code
+      ];
     in
     {
-      packages = nixpkgs.lib.genAttrs systems (system:
-        let pkgs = pkgsFor system; in
+      packages = nixpkgs.lib.genAttrs systems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
         {
           neovimDeps = pkgs.buildEnv {
             name = "neovim-deps";
-            paths = with pkgs; [
-              nil
-              nodejs
-              ripgrep
-              fd
-              git
-              tree-sitter
-            ];
+            paths = packages pkgs;
           };
         }
       );
 
-      homeManagerModules.default = { config, pkgs, lib, ... }:
+      devShells = nixpkgs.lib.genAttrs systems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = packages pkgs;
+          };
+        }
+      );
+
+      homeManagerModules.default =
+        {
+          config,
+          pkgs,
+          lib,
+          ...
+        }:
         let
           deps = self.packages.${pkgs.system}.neovimDeps;
         in
