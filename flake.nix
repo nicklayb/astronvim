cb6d499e4d582049a3ba19db39b6e31c8f8d2348
nicklayb <nicklay@me.com>
2026-02-13 13:08:30 -0500

    [v5-flake] Attempt

--- flake.nix
+++ flake.nix
@@ -6,37 +6,41 @@
 
   outputs = { self, nixpkgs, flake-utils }:
 
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.default = pkgs.buildEnv {
-          name = "neovim-deps";
-          paths = with pkgs; [
-            nil
-            nodejs
-            ripgrep
-            fd
-            git
-            tree-sitter
-          ];
-        };
-
-        homeManagerModules.default = { config, pkgs, lib, ... }:
-          let
-            deps = self.packages.${pkgs.system}.default;
-          in
-          {
-            home.packages = [
-              deps
+    let
+      systems = flake-utils.lib.systems.flakeExposed;
+      pkgsFor = system: import nixpkgs { inherit system; };
+    in
+    {
+      packages = flake-utils.lib.genAttrs systems (system:
+        let pkgs = pkgsFor system; in
+        {
+          neovimDeps = pkgs.buildEnv {
+            name = "neovim-deps";
+            paths = with pkgs; [
+              nil
+              nodejs
+              ripgrep
+              fd
+              git
+              tree-sitter
             ];
-
-            xdg.configFile."nvim" = {
-              source = lib.cleanSource self;
-              recursive = true;
-            };
           };
-      }
-    );
+        }
+      );
+
+      homeManagerModules.default = { config, pkgs, lib, ... }:
+        let
+          deps = self.packages.${pkgs.system}.neovimDeps;
+        in
+        {
+          home.packages = [
+            deps
+          ];
+
+          xdg.configFile."nvim" = {
+            source = lib.cleanSource self;
+            recursive = true;
+          };
+        };
+    };
 }
