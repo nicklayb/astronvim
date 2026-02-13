662883f2731d9b94029a9a153e75fcba5a574464
nicklayb <nicklay@me.com>
2026-02-13 13:09:59 -0500

    [v5-flake] Attempt

--- flake.nix
+++ flake.nix
@@ -1,17 +1,16 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs }:
 
     let
-      systems = flake-utils.lib.systems.flakeExposed;
+      systems = nixpkgs.lib.systems.flakeExposed;
       pkgsFor = system: import nixpkgs { inherit system; };
     in
     {
-      packages = flake-utils.lib.genAttrs systems (system:
+      packages = nixpkgs.lib.genAttrs systems (system:
         let pkgs = pkgsFor system; in
         {
           neovimDeps = pkgs.buildEnv {
